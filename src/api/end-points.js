56a4443e3f910d822bb4a568161c9f998f486afb
Mahesh-Abeykoon <mahesh@nimidev.com>
2025-02-22 22:06:36 +0530

    add items & cart endpoints

--- src/api/end-points.js
+++ src/api/end-points.js
@@ -5,5 +5,19 @@ export const END_POINTS = {
         LOGOUT: '/api/users/logout',
         LOGOUT_ALL: '/api/users/logoutAll',
         PROFILE: '/api/users/profile'
-    }
+    },
+    ITEMS: {
+        CREATE: '/api/items',
+        GET_ALL: '/api/items',
+        GET_BY_ID: '/api/items/:id',
+        UPDATE: '/api/items/:id',
+        DELETE: '/api/items/:id',
+      },
+    CART: {
+        GET: '/api/cart',
+        ADD: '/api/cart/add',
+        UPDATE: '/api/cart/update',
+        REMOVE: '/api/cart/remove/:productId',
+        CLEAR: '/api/cart/clear',
+      },
 };
