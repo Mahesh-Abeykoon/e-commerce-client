1bcf75de8bec9708b2e00702948b80ec22ef6fd0
Mahesh-Abeykoon <mahesh@nimidev.com>
2025-02-22 14:08:24 +0530

    add API endpoint constants for authentication

---
+++ src/api/end-points.js
@@ -0,0 +1,9 @@
+export const END_POINTS = {
+    AUTH: {
+        SIGN_UP: '/api/users/register',
+        SIGN_IN: '/api/users/login',
+        LOGOUT: '/api/users/logout',
+        LOGOUT_ALL: '/api/users/logoutAll',
+        PROFILE: '/api/users/profile'
+    }
+};
